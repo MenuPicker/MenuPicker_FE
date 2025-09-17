7bbd547d21434e53699470031310609daa853d1f
nyeonseok <aragorn2358@naver.com>
2025-09-17 17:57:05 +0900

    feat: globallayout 생성, 너비, 높이 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Index from "./page/Index";
+import GlobalLayout from "./layout/GlobalLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/" element={<GlobalLayout />}>
+        <Route index element={<Index />} />
+      </Route>
     </Routes>
   );
 }
