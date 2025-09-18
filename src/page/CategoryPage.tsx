89338923e71332ce91ac8c1a13730021f2d5fb52
nyeonseok <aragorn2358@naver.com>
2025-09-18 17:54:48 +0900

    feat: 컴포넌트 배치

--- src/page/CategoryPage.tsx
+++ src/page/CategoryPage.tsx
@@ -1,6 +1,8 @@
 import { IoIosArrowBack } from "react-icons/io";
 import categoryStyle from "../css/page/categoryPage.module.css";
 import { useNavigate } from "react-router-dom";
+import foodIcon from "../assets/food-icon.png";
+import drinkIcon from "../assets/drink-icon.png";
 
 export default function CategoryPage() {
   const navigate = useNavigate();
@@ -16,8 +18,22 @@ export default function CategoryPage() {
       </header>
       <div className={categoryStyle.content}>
         <div className={categoryStyle.selectDiv}>
-          <div className={categoryStyle.foodDiv}></div>
-          <div className={categoryStyle.drinkDiv}></div>
+          <div className={categoryStyle.foodDiv}>
+            <img
+              src={foodIcon}
+              alt="음식 아이콘"
+              className={categoryStyle.foodIcon}
+            />
+            <span>식당 찾기</span>
+          </div>
+          <div className={categoryStyle.drinkDiv}>
+            <img
+              src={drinkIcon}
+              alt="술집 아이콘"
+              className={categoryStyle.drinkIcon}
+            />
+            <span>술집 찾기</span>
+          </div>
         </div>
       </div>
     </div>
