a9cd5fdd2e8ce7cdfc0a27a9cd4e69c901511c3f
nyeonseok <aragorn2358@naver.com>
2025-09-18 11:01:27 +0900

    feat: index.module.css 생성 및 import

--- src/page/Index.tsx
+++ src/page/Index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QRModal from "../components/QR_Modal";
 import { isMobile } from "react-device-detect";
+import indexStyle from "../css/page/index.module.css";
 
 export default function Index() {
   const [isOpen, setIsOpen] = useState(false);
