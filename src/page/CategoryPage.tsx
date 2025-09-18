c88681747c9bcb32b651fbf44a097ec332aaa657
nyeonseok <aragorn2358@naver.com>
2025-09-18 17:26:48 +0900

    feat: 전반적인 틀 구현

--- src/page/CategoryPage.tsx
+++ src/page/CategoryPage.tsx
@@ -1,3 +1,25 @@
+import { IoIosArrowBack } from "react-icons/io";
+import categoryStyle from "../css/page/categoryPage.module.css";
+import { useNavigate } from "react-router-dom";
+
 export default function CategoryPage() {
-  return <div>category</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={categoryStyle.pageContainer}>
+      <header className={categoryStyle.header}>
+        <IoIosArrowBack
+          className={categoryStyle.backIcon}
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
+      </header>
+      <div className={categoryStyle.content}>
+        <div className={categoryStyle.selectDiv}>
+          <div className={categoryStyle.foodDiv}></div>
+          <div className={categoryStyle.drinkDiv}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
