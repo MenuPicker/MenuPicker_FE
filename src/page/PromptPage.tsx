c1dd8669ee7fe442375760fb072feeea1853fd8b
nyeonseok <aragorn2358@naver.com>
2025-09-19 16:54:12 +0900

    feat: UI구현

--- src/page/PromptPage.tsx
+++ src/page/PromptPage.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import promptStyle from "../css/page/promptPage.module.css";
 
 export default function PromptPage() {
+  const [textArea, setTextArea] = useState<string>("");
+  const [isDisabled, setIsisabled] = useState<boolean>(true);
+  const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setTextArea(value);
+    setIsisabled(value === "");
+  };
   return (
     <div className={promptStyle.pageContainer}>
       <Header />
-      PromptPage
+      <div className={promptStyle.content}>
+        <div className={promptStyle.textDiv}>
+          <span className={promptStyle.largeText}>
+            <span>마지막</span> 단계에요!
+          </span>
+          <span className={promptStyle.smallText}>
+            원하는 음식, 분위기 등을 적어주세요
+          </span>
+        </div>
+        <textarea
+          value={textArea}
+          placeholder="ex) 분위기가 좋고 맛있는 꼬치를 파는 이자카야"
+          className={promptStyle.textarea}
+          onChange={(e) => handleTextArea(e)}
+        />
+        <div className={promptStyle.btnDiv}>
+          <button onClick={() => {}} disabled={isDisabled}>
+            제출하기
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
