3429baa99ba5e59619f4c09b7de7a26f488fdaf3
nyeonseok <aragorn2358@naver.com>
2025-09-19 11:47:40 +0900

    feat: 위도, 경도로 주소 불러오는 api 구현

--- src/page/AddressPage.tsx
+++ src/page/AddressPage.tsx
@@ -10,18 +10,14 @@ import { useNavigate } from "react-router-dom";
 export default function AddressPage() {
   const [inputValue, setInputValue] = useState("");
   const [results, setResults] = useState<any[]>([]);
-  const [errorMsg, setErrorMsg] = useState("");
   const [showLocation, setShowLocation] = useState(false);
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  const [address, setAddress] = useState("");
+
   const handleAddress = async () => {
-    if (inputValue.trim() === "") {
-      setErrorMsg("주소를 입력해주세요.");
-      return;
-    }
-    setErrorMsg("");
     try {
       const response = await fetch(
         `https://dapi.kakao.com/v2/local/search/keyword.json?query=${inputValue}`,
@@ -50,6 +46,7 @@ export default function AddressPage() {
         const { latitude, longitude } = position.coords;
         console.log("위도:", latitude, "경도:", longitude);
         // 여기서 주소 API 호출 가능
+        getAddressFromCoords(latitude, longitude);
       },
       (error) => {
         console.error(error);
@@ -58,12 +55,43 @@ export default function AddressPage() {
       }
     );
   };
+  const getAddressFromCoords = async (lat: number, lng: number) => {
+    try {
+      const response = await fetch(
+        `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lng}&y=${lat}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      if (data.documents && data.documents.length > 0) {
+        const address = data.documents[0].address.address_name;
+        console.log("현재 주소:", address);
+        return address;
+      } else {
+        console.log("주소를 찾을 수 없습니다.");
+        return "";
+      }
+    } catch (err) {
+      console.error(err);
+      return "";
+    }
+  };
+
   const handleBackBtn = () => {
     if (showLocation) {
       setShowLocation(false);
       return;
     }
     navigate(-1);
+  };
+
+  const handleMyAddress = (address: string) => {
+    setAddress(address);
   };
   return (
     <div className={addressStyle.pageContainer}>
@@ -106,14 +134,18 @@ export default function AddressPage() {
         </div>
       </header>
       <div className={addressStyle.content}>
-        {/* <ul className={addressStyle.resultList}>
+        <ul className={addressStyle.resultList}>
           {results.map((item, idx) => (
-            <li key={idx}>{item.address_name}</li>
+            <li
+              key={idx}
+              onClick={() => {
+                handleMyAddress(item.address_name);
+              }}
+            >
+              {item.address_name}
+            </li>
           ))}
-        </ul> */}
-        {/* <div className={addressStyle.textDiv}>
-          <span>또는</span>
-        </div> */}
+        </ul>
       </div>
     </div>
   );
