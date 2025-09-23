fc045e841be8a35e932ddb145e60c180b273afac
nyeonseok <aragorn2358@naver.com>
2025-09-23 15:42:04 +0900

    feat: result 페이지 구현

--- src/page/ResultPage.tsx
+++ src/page/ResultPage.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Header from "../components/Header";
 import resultStyle from "../css/page/resultPage.module.css";
 import { useEffect, useState } from "react";
@@ -20,6 +20,7 @@ export default function ResultPage() {
   const reason = searchParams.get("resultReason");
   const [result, setResult] = useState<ResultPlace | null>(null);
   const [naverQuery, setNaverQuery] = useState("");
+  const navigate = useNavigate();
 
   const fetchResultPlace = async () => {
     try {
@@ -65,17 +66,27 @@ export default function ResultPage() {
   return (
     <div className={resultStyle.pageContainer}>
       <Header />
+
       <div className={resultStyle.content}>
-        <div className={resultStyle.content}>
-          {result && (
-            <div>
-              <h2>{result.name}</h2>
-              <p>{result.address}</p>
-              <p>{result.phone}</p>
-              <a href={result.kakaoURL} target="_blank" rel="noreferrer">
-                카카오맵에서 보기
-              </a>
-              {/* {isMobile === true ? (
+        {result && (
+          <div className={resultStyle.resultDiv}>
+            <span className={resultStyle.name}>{result.name}</span>
+            <span className={resultStyle.address}>주소: {result.address}</span>
+            <span className={resultStyle.phone}>번호: {result.phone}</span>
+            {/* <span className={resultStyle.name}>청담이상 대구 동성로점</span>
+            <span className={resultStyle.address}>
+              대구 중구 삼덕동 2가 30-4
+            </span>
+            <span className={resultStyle.phone}>053-423-2353</span> */}
+            <a
+              className={resultStyle.kakaoMapBtn}
+              href={result.kakaoURL}
+              target="_blank"
+              rel="noreferrer"
+            >
+              카카오맵에서 보기
+            </a>
+            {/* {isMobile === true ? (
                 <a href={`nmap://search?query=${naverQuery}`}>
                   네이버 지도에서 보기
                 </a>
@@ -84,8 +95,24 @@ export default function ResultPage() {
                   네이버 지도에서 보기
                 </a>
               )} */}
+            <div className={resultStyle.reasonDiv}>
+              <span>AI 선택 이유</span>
+              <span className={resultStyle.reason}>{reason}</span>
+              {/* <span className={resultStyle.reason}>
+                이자카야로 분류되지만 치킨 가라아게 등 치킨 메뉴가 있고,
+                분위기가 좋으며 해당 위치에서 매우 가깝습니다.
+              </span> */}
             </div>
-          )}
+          </div>
+        )}
+        <div className={resultStyle.btnDiv}>
+          <button
+            onClick={() => {
+              navigate("/", { replace: true });
+            }}
+          >
+            첫 화면으로
+          </button>
         </div>
       </div>
     </div>
