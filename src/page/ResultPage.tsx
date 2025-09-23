8d8dfd0d8d744016ce6aea2dc54d2c60bd43e38e
nyeonseok <aragorn2358@naver.com>
2025-09-23 10:48:48 +0900

    test: fixed로 변경

--- src/page/ResultPage.tsx
+++ src/page/ResultPage.tsx
@@ -1,3 +1,11 @@
+import Header from "../components/Header";
+import resultStyle from "../css/page/resultPage.module.css";
+
 export default function ResultPage() {
-  return <div>ResultPage</div>;
+  return (
+    <div className={resultStyle.pageContainer}>
+      <Header />
+      <div className={resultStyle.content}></div>
+    </div>
+  );
 }
